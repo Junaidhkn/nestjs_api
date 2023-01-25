7f526ae3560654cec9d9dd8d3b15735e6205719d
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-25 12:58:33 +0500

    basic crud

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +18,20 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This route returns single specific Item of id ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This route returns updated single specific Item of id ${id}`;
+  }
+
+  @Delete('id')
+  remove(@Param('id') id: string) {
+    return `This action removes item with the id of ${id}`;
   }
 }
