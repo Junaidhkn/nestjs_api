3611c2bdc3f36e4cf99727f196712374c054312f
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-08 01:36:18 +0500

    Validation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Would not send any addition Data that is not part of a DTO
+      forbidNonWhitelisted: true, // Would Fail request if Data is not part of a DTO
+    }),
+  );
   await app.listen(8000);
 }
 bootstrap();
