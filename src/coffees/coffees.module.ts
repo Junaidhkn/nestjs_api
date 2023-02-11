53eb6b38f4eab6bdd3911c14c1d910657b5a9229
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-12 04:43:25 +0500

    Added Prisma Sevice

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CofeesService } from 'src/cofees/cofees.service';
+import { PrismaService } from 'src/prisma.service';
 import { CoffeesController } from './coffees.controller';
 
 @Module({
   controllers: [CoffeesController],
-  providers: [CofeesService],
+  providers: [PrismaService, CofeesService],
 })
 export class CoffeesModule {}
