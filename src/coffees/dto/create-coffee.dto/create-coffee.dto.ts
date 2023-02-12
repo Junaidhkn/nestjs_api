0860f582086f6b5d243c5a718fd4177f4e48bf2d
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-12 10:54:10 +0500

    commit

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,10 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
   @IsString()
   readonly name: string;
+
   @IsString()
   readonly brand: string;
+
   @IsString({ each: true })
+  @IsOptional()
   readonly flavours: string[];
 }
